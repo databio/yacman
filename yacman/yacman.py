c47ba367049ebc10be42cef2d797113e5b4f9c64
Vince Reuter <vince.reuter@gmail.com>
2019-05-24 17:30:44 -0400

    remove unused param in config selection signature; tidy

--- yacman/yacman.py
+++ yacman/yacman.py
@@ -1,9 +1,11 @@
 import attmap
 import os
+from collections import Iterable
 import oyaml as yaml
 import logging
 
 _LOGGER = logging.getLogger(__name__)
+
 
 class YacAttMap(attmap.PathExAttMap):
     """
@@ -17,7 +19,7 @@ class YacAttMap(attmap.PathExAttMap):
             # self._file_path = entries
             # self._excl_from_repr("_file_path")
             entries = load_yaml(entries)
-        return super(YacAttMap, self).__init__(entries or {})
+        super(YacAttMap, self).__init__(entries or {})
 
     def write(self, filename):
         with open(filename, 'w') as f:
@@ -46,25 +48,24 @@ def get_first_env_var(ev):
     """
     Get the name and value of the first set environment variable
 
-    :param ev: a list of the environment variable names
-    :type: list[str] | str
-    :return: name and the value of the environment variable
-    :rtype: list
+    :param str | Iterable[str] ev: a list of the environment variable names
+    :return (str, str): name and the value of the environment variable
     """
-    if not isinstance(ev, list):
-        if isinstance(ev, str):
-            ev = [ev]
-        else:
-            raise TypeError("The argument has to be a list or string.")
-    for i in ev:
-        if os.getenv(i, False):
-            return [i, os.getenv(i)]
+    if isinstance(ev, str):
+        ev = [ev]
+    elif not isinstance(ev, Iterable):
+        raise TypeError("Env var must be single name or collection of names; "
+                        "got {}".format(type(ev)))
+    # TODO: we should handle the null (not found) case, as client code is inclined to unpack, and ValueError guard is vague.
+    for v in ev:
+        try:
+            return v, os.environ[v]
+        except KeyError:
+            pass
 
 
-def select_config(config_filepath=None, 
-                    config_env_vars=None, 
-                    config_name="config file", 
-                    default_config_filepath=None):
+def select_config(config_filepath=None, config_env_vars=None,
+                  default_config_filepath=None):
     """
     Selects the config file to load.
 
@@ -103,4 +104,3 @@ def select_config(config_filepath=None,
             _LOGGER.error("No configuration file found.")
 
     return selected_filepath
-    
