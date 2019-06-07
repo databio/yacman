447b23037ed5141afd5100d3f8ed04d3068ae9e0
Vince <vince.reuter@gmail.com>
2019-06-07 17:28:30 -0400

    release prep

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     version=version,
     description='A standardized configuration object for reference genome assemblies',
     long_description=long_description,
+    long_description_content_type='text/markdown', 
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
