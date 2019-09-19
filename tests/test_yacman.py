c19c44027fc0654498e7d8f29ebeced92e661aad
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-09-19 14:30:44 -0400

    safe writes in multi-user context; implement locks, racefree writes; #12

--- tests/test_yacman.py
+++ tests/test_yacman.py
@@ -1,12 +1,14 @@
 import pytest
 import yacman
 import os
+from glob import glob
 
 # for interactive testing, run from within code/yacman
 
+
 def test_yaml_load():
     conf = yacman.load_yaml("conf.yaml")
-    yacmap = yacman.YacAttMap("conf.yaml")
+    yacmap = yacman.YacAttMap("conf.yaml", ro=False)
     yacmap2 = yacman.YacAttMap(conf)
 
     # Not sure why these are not equivalent:
@@ -14,7 +16,6 @@ def test_yaml_load():
 
     # but they are at the yaml level:
     assert(yacmap.to_yaml() == yacmap2.to_yaml())
-
     yacmap
 
     # round trip
@@ -23,33 +24,20 @@ def test_yaml_load():
     obj = yacman.YacAttMap(test_yaml_filename)
     assert(yacmap.to_yaml() == obj.to_yaml())
     os.remove(test_yaml_filename)
-
     print(yacmap.to_yaml())
     assert(yacmap.genome_folder == os.path.expandvars("$GENOMES"))
     assert(yacmap2.genome_folder == os.path.expandvars("$GENOMES"))
     assert(yacmap.to_dict()["genome_folder"] == "$GENOMES")
     print(yacmap.genomes.to_yaml())
-
-
     yacmap._file_path
-
-
-def test_entries_update():
-    conf = yacman.load_yaml("conf.yaml")
-    yacmap = yacman.YacAttMap("conf.yaml")
-    yacmap = yacman.YacAttMap(filepath="conf.yaml")
-    yacmap = yacman.YacAttMap({"entry": "updated"}, filepath="conf.yaml")
-    yacmap = yacman.YacAttMap({"genome_folder": "updated"}, filepath="conf.yaml")
-    assert(yacmap.genome_folder == "updated")
-    yacmap = yacman.YacAttMap()
-
+    cleanup_locks()
 
 
 def test_write():
-    yacmap = yacman.YacAttMap("conf.yaml")
+    yacmap = yacman.YacAttMap("conf.yaml", ro=False)
     yacmap.write("writeout.yaml")
 
-    yacmapin = yacman.YacAttMap("writeout.yaml")
+    yacmapin = yacman.YacAttMap("writeout.yaml", ro=False)
     yacmapin.newattr = "value"
     yacmapin.write()
     yacmapin2 = yacman.YacAttMap("writeout.yaml")
@@ -60,6 +48,18 @@ def test_write():
     yacmap_nofile = yacman.YacAttMap(conf)
     with pytest.raises(Exception):
         yacmap_nofile.write()
+    cleanup_locks()
+
+
+def test_entries_update():
+    conf = yacman.load_yaml("conf.yaml")
+    yacmap = yacman.YacAttMap("conf.yaml", ro=True)
+    yacmap = yacman.YacAttMap(filepath="conf.yaml", ro=True)
+    yacmap = yacman.YacAttMap({"entry": "updated"}, filepath="conf.yaml", ro=True)
+    yacmap = yacman.YacAttMap({"genome_folder": "updated"}, filepath="conf.yaml", ro=True)
+    assert(yacmap.genome_folder == "updated")
+    yacmap = yacman.YacAttMap()
+    cleanup_locks()
 
 
 yaml_str = """\
@@ -68,8 +68,8 @@ one: 1
 2: two
 """
 
-def test_float_idx():
 
+def test_float_idx():
     data = yacman.YacAttMap(yamldata=yaml_str)
     # We should be able to access this by string, not by int index.
     assert(data['2'] == "two")
@@ -77,86 +77,40 @@ def test_float_idx():
         data[2]
 
 
-# import yacman
-# conf = yacman.load_yaml("conf.yaml")
-# conf
-# attmap.OrdAttMap(conf)
+def get_locks():
+    return glob("lock.*")
 
 
-# y = load_yaml("/home/nsheff/Dropbox/env/bulker_config/puma.yaml")
-
-# f = "/home/nsheff/Dropbox/env/bulker_config/puma.yaml"
-# import yacman
-# y2 = yacman.YacAttMap(f)
-# y2["bulker"]["crates"]["databio"]["lab"]["1.0"]
+def cleanup_locks():
+    lcks = get_locks()
+    if lcks:
+        [os.remove(l) for l in lcks]
 
 
+@pytest.mark.parametrize("name", ["test.yaml", "test1.yaml"])
+def test_cant_write_when_locked(name):
+    yacmap = yacman.YacAttMap({"test": "data"})
+    write_lock_flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
+    fd = os.open("lock." + name, write_lock_flags)
+    os.close(fd)
+    with pytest.raises(OSError):
+        yacmap.write(name)
+    cleanup_locks()
 
 
-# yaml_str = """\
-# ---
-# one: 1
-# 2: two
-# """
-# import yaml
-# import inspect
-# inspect.getsourcelines(yaml.SafeLoader.construct_mapping)
-# yaml.safe_load(yaml_str)
+def test_cant_write_ro_mode(name="conf.yaml"):
+    yacmap = yacman.YacAttMap(name, ro=True)
+    with pytest.raises(OSError):
+        yacmap.write(name)
+    cleanup_locks()
 
 
-# def my_construct_mapping(self, node, deep=False):
-#     data = self.construct_mapping_org(node, deep)
-#     return {(str(key) if isinstance(key, float) or isinstance(key, int) else key): data[key] for key in data}
+def test_lock_created_and_removed_after_successful_write(name="conf.yaml"):
+    yacmap = yacman.YacAttMap(name, ro=False)
+    assert len(get_locks()) != 0
+    assert os.path.exists("lock." + name)
+    yacmap.write()
+    assert not os.path.exists("lock." + name)
+    cleanup_locks()
 
-# def construct_pairs(self, node, deep=False):
-#     # if not isinstance(node, MappingNode):
-#     #     raise ConstructorError(None, None,
-#     #             "expected a mapping node, but found %s" % node.id,
-#     #             node.start_mark)
-#     pairs = []
-#     for key_node, value_node in node.value:
-#         key = str(self.construct_object(key_node, deep=deep))
-#         value = self.construct_object(value_node, deep=deep)
-#         pairs.append((key, value))
-#     return pairs
-
-# yaml.SafeLoader.construct_mapping_org = yaml.SafeLoader.construct_mapping
-# yaml.SafeLoader.construct_mapping = my_construct_mapping
-# yaml.SafeLoader.construct_pairs = construct_pairs
-
-
-
-# yaml.safe_load(yaml_str)
-
-
-# import oyaml
-# inspect.getsourcelines(oyaml.SafeLoader.construct_mapping)
-
-
-# def map_constructor2(loader, node):
-#     loader.flatten_mapping(node)
-#     pairs = loader.construct_pairs(node)
-
-#     try:
-#         return OrderedDict(pairs)
-#     except TypeError:
-#         loader.construct_mapping(node)  # trigger any contextual error
-#         raise
-
-# pyyaml.add_constructor("tag:yaml.org,2002:map", map_constructor, Loader=yaml.SafeLoader)
-
-# oyaml.safe_load(yaml_str)
-# yaml.safe_load(yaml_str)
-
-
-
-
-# yaml.safe_load(yaml_str)
-
-
-# import ruamel.yaml
-# from ruamel.yaml import YAML
-# yaml = YAML()
-
-# yaml.load(yaml_str)
 
--- yacman/yacman.py
+++ yacman/yacman.py
@@ -3,12 +3,18 @@ import os
 from collections import Iterable
 import oyaml as yaml
 import logging
+import errno
+import time
+import sys
 from ubiquerg import mkabs
+
 
 _LOGGER = logging.getLogger(__name__)
 
 
 FILEPATH_KEY = "_file_path"
+RO_KEY = "_ro"
+LOCK_PREFIX = "lock."
 
 
 ### Hack for string indexes of both ordered and unordered yaml representations
@@ -62,19 +68,36 @@ class YacAttMap(attmap.PathExAttMap):
     :param str filepath: YAML filepath to the config file.
     """
 
-    def __init__(self, entries=None, filepath=None, yamldata=None):
+    def __init__(self, entries=None, filepath=None, yamldata=None, ro=False, wait_max=100):
 
         if isinstance(entries, str):
             # If user provides a string, it's probably a filename we should read
             # This should be removed at a major version release now that the
             # filepath argument exists, but we retain it for backwards
             # compatibility
-            _LOGGER.debug("The entries argument should be a dict. If you want"
-            " to read a file, use the filepath arg")
+            _LOGGER.debug("The entries argument should be a dict. If you want to read a file, use the filepath arg")
             filepath = entries
             entries = None
 
         if filepath:
+            setattr(self, RO_KEY, ro)
+            # If user provides a string, it's probably a filename we should read
+            # Check if user intends to update the file
+            if not getattr(self, RO_KEY):
+                # attempt to lock the file
+                lock_path = _make_lock_path(filepath)
+                if os.path.exists(lock_path):
+                    raise OSError("The file is locked: {}".format(filepath))
+                else:
+                    try:
+                        _create_file_racefree(lock_path)
+                    except OSError as e:
+                        if e.errno == errno.EEXIST:
+                            # Rare case: file already exists;
+                            # the lock has been created in the split second since the last lock existence check,
+                            # wait for the lock file to be gone, but no longer than `wait_max`.
+                            print("Could not create a lock file, it already exists: {}".format(lock_path))
+                            _wait_for_lock(lock_path, wait_max)
             file_contents = load_yaml(filepath)
             if entries:
                 file_contents.update(entries)
@@ -89,11 +112,29 @@ class YacAttMap(attmap.PathExAttMap):
             setattr(self, FILEPATH_KEY, mkabs(filepath))
 
     def write(self, filename=None):
+        """
+        Writes the contents to a file.
+
+        Makes sure that the object has been created with write capabilities
+
+        :param str filename: a file path to write to
+        :raise OSError: when the object has been created in a read only mode or other process has locked the file
+        :return str: the path to the created file
+        """
+        if hasattr(self, RO_KEY) and getattr(self, RO_KEY):
+            raise OSError("You can't write to a file that was read in RO mode.")
         filename = filename or getattr(self, FILEPATH_KEY)
         if not filename:
             raise Exception("No filename provided.")
+        lock = _make_lock_path(filename)
+        if not hasattr(self, RO_KEY) and os.path.exists(lock):
+            # if the object hasn't been created by reading from file, but a lock exists
+            raise OSError("You can't write to a file that was locked by a different process.")
         with open(filename, 'w') as f:
             f.write(self.to_yaml())
+        if os.path.exists(lock):
+            # remove the lock after writing
+            os.remove(lock)
         return os.path.abspath(filename)
 
     @property
@@ -101,22 +142,78 @@ class YacAttMap(attmap.PathExAttMap):
         """ Most specific type to which an inserted value may be converted """
         return YacAttMap
 
-    def _excl_from_repr(self, k, cls):
-        return k == FILEPATH_KEY
-
     def __repr__(self):
         # Here we want to render the data in a nice way; and we want to indicate
         # the class if it's NOT a YacAttMap. If it is a YacAttMap we just want
         # to give you the data without the class name.
-        return self._render(self._simplify_keyvalue(
-            self._data_for_repr(), self._new_empty_basic_map),
-            exclude_class_list="YacAttMap")
+        return self._render(self._simplify_keyvalue(self._data_for_repr(), self._new_empty_basic_map),
+                            exclude_class_list="YacAttMap")
+
+    def _excl_from_repr(self, k, cls):
+        return k in (FILEPATH_KEY, RO_KEY)
+
+
+def _wait_for_lock(lock_file, wait_max):
+    """
+    Just sleep until the lock_file does not exist or a lock_file-related dynamic recovery flag is spotted
+
+    :param str lock_file: Lock file to wait upon.
+    """
+    sleeptime = .5
+    first_message_flag = False
+    dot_count = 0
+    totaltime = 0
+    while os.path.isfile(lock_file):
+        if first_message_flag is False:
+            print("Waiting for file lock: " + lock_file)
+            first_message_flag = True
+        else:
+            sys.stdout.write(".")
+            dot_count = dot_count + 1
+            if dot_count % 60 == 0:
+                sys.stdout.write("")
+        time.sleep(sleeptime)
+        totaltime += sleeptime
+        sleeptime = min(sleeptime + 2.5, 20)
+        if totaltime >= wait_max:
+            raise RuntimeError("The maximum wait time has been reached")
+
+    if first_message_flag:
+        print("File unlocked")
+
+
+def _create_file_racefree(file):
+    """
+    Creates a file, but fails if the file already exists.
+
+    This function will thus only succeed if this process actually creates the file;
+    if the file already exists, it will cause an OSError, solving race conditions.
+
+    :param str file: File to create.
+    """
+    write_lock_flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
+    fd = os.open(file, write_lock_flags)
+    os.close(fd)
+    return file
+
+
+def _make_lock_path(lock_name_base):
+    """
+    Create path to lock file with given name as base.
+
+    :param str lock_name_base: Lock file name
+    :return str: Path to the lock file.
+    """
+    base, name = os.path.split(lock_name_base)
+    lock_name = name if name.startswith(LOCK_PREFIX) else LOCK_PREFIX + name
+    return lock_name if not base else os.path.join(base, lock_name)
 
 
 def load_yaml(filename):
     with open(filename, 'r') as f:
         data = yaml.safe_load(f)
     return data
+
 
 def get_first_env_var(ev):
     """
@@ -192,20 +289,3 @@ def select_config(config_filepath=None,
         _LOGGER.error("No configuration file found.")
 
     return selected_filepath
-
-
-def single_folder_writeable(d):
-    return os.access(d, os.W_OK) and os.access(d, os.X_OK)
-
-
-def writeable(outdir, strict_exists=False):
-    """
-    Recursively checks to make sure a folder exists and can be  written to.  
-    """
-    outdir = outdir or "."
-    if os.path.exists(outdir):
-        return _single_folder_writeable(outdir)
-    elif strict_exists:
-        raise MissingFolderError(outdir)
-    return writeable(os.path.dirname(outdir), strict_exists)
-
