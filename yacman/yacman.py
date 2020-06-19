8f88c6979fb1d2364663fc78bbbd9020614c6cc1
nsheff <nsheff@users.noreply.github.com>
2020-06-19 09:11:30 -0400

    increase wait default

--- yacman/yacman.py
+++ yacman/yacman.py
@@ -272,7 +272,7 @@ def _check_filepath(filepath):
     return filepath
 
 
-def _make_rw(filepath, wait_max=10):
+def _make_rw(filepath, wait_max=30):
     # attempt to lock the file
     lock_path = make_lock_path(filepath)
     if os.path.exists(lock_path):
