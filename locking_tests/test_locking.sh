d115bf0c56c5bde0671be1b7888e26934ce6dd5c
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-06-29 16:26:06 -0400

    improve locking test master script
    wait for slave processes to finish and verify the outcome

--- locking_tests/test_locking.sh
+++ locking_tests/test_locking.sh
@@ -15,5 +15,24 @@ touch test.yaml
 for (( i=1; i<=$1; i++ ))
 do
 	echo "submitting: $i"
-   ./test_locking.py --id $i --path test.yaml --wait $2&
+	./test_locking.py --id $i --path test.yaml --wait $2&
+	pids[${i}]=$!
 done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+echo "finished!"
+
+file_lines=$(cat test.yaml | wc -l)
+diff=$(($1 - file_lines))
+
+if (( $diff > 0 )); then
+	echo "ERROR: $diff entries have been lost!"
+	exit 1
+else
+	echo "SUCCESS: $1 lines present in the file"
+	exit 0
+fi
