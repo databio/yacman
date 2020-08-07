6964de1e8b1f7ef2de14958d7ebd8fcb951e9bc0
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-08-07 16:16:19 -0400

    no const in test parametrization

--- tests/test_yacman_alias.py
+++ tests/test_yacman_alias.py
@@ -2,12 +2,10 @@ import pytest
 import yacman
 import os
 
-TEST_LIST = [
-    ({"a": "1"}, {"a": ["alias_a"]}),
-    ({"b": "1"}, {"b": ["alias_b"]}),
-    ({"55": "1"}, {"55": ["alias_55"]})
-]
-
+# TEST_LIST = [
+#     ({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})
+# ]
+#
 TEST_LIST_MULTI = [
     ({"a": "1"}, {"a": ["alias_a", "a_a"]}),
     ({"b": "1"}, {"b": ["alias_b", "a_b"]}),
@@ -16,14 +14,14 @@ TEST_LIST_MULTI = [
 
 
 class TestAliases:
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_aliases_init(self, entries, aliases):
         x = yacman.AliasedYacAttMap(entries=entries, aliases=aliases)
         key = list(entries.keys())[0]
         alias = list(aliases.values())[0][0]
         assert x[key] == x[alias]
 
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_aliases_setting(self, entries, aliases):
         x = yacman.AliasedYacAttMap(entries=entries)
         key = list(aliases.keys())[0]
@@ -31,7 +29,7 @@ class TestAliases:
         assert x.set_aliases(key=key, aliases=alias)
         assert x[key] == x[alias[0]]
 
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_aliases_setting_append(self, entries, aliases):
         x = yacman.AliasedYacAttMap(entries=entries)
         key = list(aliases.keys())[0]
@@ -40,7 +38,7 @@ class TestAliases:
         assert x.set_aliases(key=key, aliases=alias + "_new")
         assert len(x.alias_dict[key]) == 2
 
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_aliases_setting_overwrite(self, entries, aliases):
         x = yacman.AliasedYacAttMap(entries=entries)
         key = list(aliases.keys())[0]
@@ -50,7 +48,7 @@ class TestAliases:
         assert len(x.alias_dict[key]) == 1
         assert x.alias_dict[key] == [alias + "_new"]
 
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_aliases_removal_all(self, entries, aliases):
         x = yacman.AliasedYacAttMap(entries=entries, aliases=aliases)
         key = list(aliases.keys())[0]
@@ -64,7 +62,7 @@ class TestAliases:
     #     x.remove_aliases(key=key, aliases=aliases[key])
     #     assert key not in x.alias_dict.keys()
 
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_accession_works_for_keys_and_aliases(self, entries, aliases):
         x = yacman.AliasedYacAttMap(entries=entries)
         key = list(aliases.keys())[0]
@@ -72,8 +70,9 @@ class TestAliases:
         assert x.set_aliases(key=key, aliases=alias)
         assert x[key] == x[alias[0]]
 
-    @pytest.mark.parametrize(["entries", "aliases"], TEST_LIST)
+    @pytest.mark.parametrize(["entries", "aliases"], [({"a": "1"}, {"a": ["alias_a"]}), ({"b": "1"}, {"b": ["alias_b"]}), ({"55": "1"}, {"55": ["alias_55"]})])
     def test_containment_checks_for_keys_and_aliases(self, entries, aliases):
+        print(f"aliases: {aliases}")
         x = yacman.AliasedYacAttMap(entries=entries)
         key = list(aliases.keys())[0]
         alias = aliases[key]
