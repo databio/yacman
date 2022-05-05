7e86682fbe1f1d32c29f486183093b87007b4514
nsheff <nsheff@users.noreply.github.com>
2022-05-05 11:03:07 -0400

    comment

--- yacman/yacman.py
+++ yacman/yacman.py
@@ -404,6 +404,9 @@ def _check_filepath(filepath):
     return filepath
 
 
+
+# Thought about moving this to ubiquerg -- but `yaml` isn't built-in
+# so it would add a new dependency there, which I'd rather not do.
 def load_yaml(filepath):
     """Load a yaml file into a python dict"""
 
