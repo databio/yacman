e55e846ab182e7086dc3079191150bc1910e4179
nsheff <nsheff@users.noreply.github.com>
2023-12-06 09:16:46 -0500

    import load_yaml

--- yacman/__init__.py
+++ yacman/__init__.py
@@ -2,6 +2,6 @@ from ._version import __version__
 from .alias import *
 
 # For transition
-from .yacman1 import YAMLConfigManager, select_config
+from .yacman1 import YAMLConfigManager, select_config, load_yaml
 
 from .yacman import *
